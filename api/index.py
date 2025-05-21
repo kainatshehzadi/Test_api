2c17a2c89d5f6784c0b13dd43fb592669545442d
kainatshehzadi <kainat.shehzadi765@gmail.com>
2025-05-21 03:41:08 -0700

    Fix  correct API path

--- api/index.py
+++ api/index.py
@@ -1,15 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from mangum import Mangum
-from http.server import BaseHTTPRequestHandler
+
 app = FastAPI()
-
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"Hello from Python on Vercel!")
 
 @app.get("/")
 def read_root():
