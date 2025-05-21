49830af8b34c1ae2f289c6c780972821f54c1f8f
kainatshehzadi <kainat.shehzadi765@gmail.com>
2025-05-21 05:11:51 -0700

    update routes

--- api/index.py
+++ api/index.py
@@ -2,20 +2,14 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from mangum import Mangum
 
-app = FastAPI()
+fastapi_app = FastAPI()
 
-def handler(request):
-    return {
-        "statusCode": 200,
-        "body": "Hello from Vercel!"
-    }
-
-@app.get("/")
+@fastapi_app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI on Vercel!"}
 
-@app.get("/test")
+@fastapi_app.get("/test")
 def test():
     return JSONResponse({"status": "Success", "info": "This is a test endpoint"})
 
-handler = Mangum(app)
+app = Mangum(fastapi_app) 
